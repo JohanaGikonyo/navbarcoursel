108b509a93a5e615c376d48506be5e1ae108fdbd
JohanaGikonyo <johanagikonyo552@gmail.com>
2023-11-05 22:06:42 +0300

    scrolls

--- navapp/src/Navbar/Products.jsx
+++ navapp/src/Navbar/Products.jsx
@@ -5,7 +5,23 @@ import './products.css';
 function Products() {
   return (
     <>
+    <div className='scroll-container'>
     <div className='products'>
+      {products.map((item) => (
+        <div key={item.id} className='eachitem'>
+          <img src={item.image} alt={item.description} />
+          <div className='descriptions'>
+            <h3>{item.description}</h3>
+            <h4>$ {item.cost}</h4>
+            <h4>{item.discount}</h4>
+            <h5>Orders left :{"  "+item.remaining}</h5>
+          </div>
+         
+        </div>
+      ))}
+    </div>
+    </div>
+      <div className='products'>
       {products.map((item) => (
         <div key={item.id} className='eachitem'>
           <img src={item.image} alt={item.description} />
